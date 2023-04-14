9c3149178d1d91e465ddb0b9293f2b941ddc6955
Jonathan Chandler <49777173+Lithovox@users.noreply.github.com>
2023-04-13 20:49:23 -0700

    Added privacy disclaimer footer

--- index.js
+++ index.js
@@ -125,10 +125,28 @@ function capitalizeFirstLetterOfEachWord(str) {
     .join(' ');
 }
 
-// Example usage
-/* const phoneNumber = "2255632"; // Example: "CALLMEA"
+function stringToPhoneNumber(input) {
+  // Phone number mapping
+  const phoneMapping = {
+    'a': '2', 'b': '2', 'c': '2',
+    'd': '3', 'e': '3', 'f': '3',
+    'g': '4', 'h': '4', 'i': '4',
+    'j': '5', 'k': '5', 'l': '5',
+    'm': '6', 'n': '6', 'o': '6',
+    'p': '7', 'q': '7', 'r': '7', 's': '7',
+    't': '8', 'u': '8', 'v': '8',
+    'w': '9', 'x': '9', 'y': '9', 'z': '9'
+  };
 
-const composedStrings = findComposedStringsFromPhoneNumber(phoneNumber, englishWords);
-console.log(composedStrings);
- */
+  // Remove non-alphanumeric characters
+  const alphanumericOnly = input.replace(/[^a-z0-9]/gi, '');
+
+  // Convert letters to their phone number equivalent
+  const phoneNumber = alphanumericOnly.split('').map(char => {
+    const lowerChar = char.toLowerCase();
+    return phoneMapping[lowerChar] || char;
+  }).join('');
+
+  return phoneNumber;
+}
 
